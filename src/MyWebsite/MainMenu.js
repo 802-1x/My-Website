1d36d2f8f575bf2025161323d37f5197b7eff14a
802.1x <96475907+802-1x@users.noreply.github.com>
2024-09-29 19:18:08 +1000

    Added salutations, named introduction, and blurb in top right of Main Menu; moved menu bars lower in proportion to top right section; added new input subfolder to abstract introduction section's input out of code; modified bar titles to left alignment with padding to resolve alignment aesthetics issue introduced by introduction section.

--- src/MyWebsite/MainMenu.js
+++ src/MyWebsite/MainMenu.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './MainMenu.css';
 import SelfHostedVideo from './MainMenuSelfHostedVideo';
+import personalData from './inputs/personalData.json';
 
-const videoSrc = "/MainMenuVideo.mp4"
+/* Ensure file names matches file located in public directory. */
+const videoSrc = "/MainMenuVideo.mp4";
+const audioSrc="/NamePronounciation.mp3";
+
+const menuItems = [
+    { text: "About", className: "plum" },
+    { text: "Academic Essays", className: "lightskyblue" },
+    { text: "Stub", className: "darkorange" },
+    { text: "Socials", className: "lightgreen" },
+];
 
 const MainMenu = () => {
-    return (
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const audioRef = useRef(null);
+
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    const playAudio = () => {
+        audioRef.current.play();
+    }
+
+return (
         <>
             <div className="video-container">
                 <SelfHostedVideo videoSrc={videoSrc} />
-            </div>  
+            </div>
+
             <div className="right-menuContainer">
-                <div className="bar light-blue">About</div>
-                <div className="bar yellow">Academic Essays</div>
-                <div className="bar beige">Stub</div>
-                <div className="bar dark-orange">Socials</div>
+                    <div className="greeting-text">
+                        <h1>Hello!</h1>
+                        <div className="greeting-myname">
+                            <p>My name is <button onClick={playAudio} className="pronounce-button">🔊</button> {personalData.name}</p>
+                        </div>
+                        <audio ref={audioRef} src={audioSrc}></audio>
+                        <div className="greeting-blurb">
+                            <p>{personalData.blurb}</p>
+                        </div>
+                    </div>
+
+                {menuItems.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <div
+                            className={`bar ${item.className} ${hoveredIndex === index ? 'shrink' : ''}`}
+                        >
+                            {item.text}
+                        </div>
+                        <div
+                            className={`invisible-overlay ${item.className}`}
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}
+                        ></div>
+                    </React.Fragment>
+                ))}
             </div>
         </>
     );
